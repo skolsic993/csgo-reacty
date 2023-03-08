a062c82e5fa5612aed326e6e4b3ddf608f91a7da
Miloš Stojsavljević <skolsic993@gmail.com>
2023-03-08 20:04:46 +0100

    Add functionality for login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,88 @@
-import { User } from "@/models/User";
 import fetcher from "@/utils/fetcher";
-import { GetServerSideProps } from "next";
-import useSwr from "swr";
+import axios from "axios";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
-export default function Home({ fallbackData }: { fallbackData: User }) {
-  const { data, error } = useSwr(
-    `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/me`,
-    fetcher,
-    { fallbackData }
-  );
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-  if (data) {
-    return <div>Welcome! {JSON.stringify(data)}</div>;
+export default function Home({
+  fallbackData,
+  something,
+}: {
+  fallbackData: any;
+  something: any;
+}) {
+  const [logoutError, setLogoutError] = useState(null);
+
+  const router = useRouter();
+
+  const logout = async () => {
+    const asd = something.cookie.split(";");
+
+    try {
+      let headers = {
+        Authorization: "Bearer " + asd[1].split("=")[1],
+      };
+
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/auth/signout`,
+        null,
+        {
+          headers,
+          withCredentials: true,
+        }
+      );
+
+      router.push("/auth/signin");
+    } catch (error: any) {
+      setLogoutError(error);
+    }
+  };
+
+  if (fallbackData) {
+    return (
+      <>
+        <>Welcome! {fallbackData && JSON.stringify(fallbackData)}</>
+        <button onClick={logout}>Logout</button>
+        {logoutError && JSON.stringify(logoutError)}
+      </>
+    );
   }
 
   return <div>Please login</div>;
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const data = await fetcher(
+export const getMe = async (context: GetServerSidePropsContext) => {
+  return await fetcher(
     `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/me`,
     context.req.headers
-  );
+  ).catch((error) => {
+    console.log(error);
+    return null;
+  });
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const something = context.req.headers;
+
+  const session = await getMe(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+      props: {
+        fallbackData: session,
+        something,
+      },
+    };
+  }
 
   return {
-    props: { fallbackData: data },
+    props: {
+      fallbackData: session,
+      something,
+    },
   };
 };
