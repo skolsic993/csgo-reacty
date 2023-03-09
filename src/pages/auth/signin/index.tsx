eaec79a349a2d0b10a849672132dd7a5a07fdf32
Miloš Stojsavljević <skolsic993@gmail.com>
2023-03-09 20:22:39 +0100

    Add new reusable component for login page

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,10 +1,9 @@
+import { Input } from "@/shared/input/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import getConfig from "next/config";
 import { useRouter } from "next/router";
 import { Button } from "primereact/button";
-import { InputText } from "primereact/inputtext";
-import { Password } from "primereact/password";
 import { classNames } from "primereact/utils";
 import { useContext, useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -13,14 +12,16 @@ import AppConfig from "../../../../layout/AppConfig";
 import { LayoutContext } from "../../../../layout/context/layoutcontext";
 
 const loginSchema = object({
-  email: string().nonempty({
-    message: "Email is required!",
-  }),
+  email: string()
+    .nonempty({
+      message: "Email is required!",
+    })
+    .email(),
   password: string()
-    .min(8, "Password is too short - should be 8 chars minimum!")
     .nonempty({
       message: "Password is required!",
-    }),
+    })
+    .min(8, "Password is too short - should be 8 chars minimum!"),
 });
 
 type LoginUserInput = TypeOf<typeof loginSchema>;
@@ -60,13 +61,13 @@ function Login() {
   const contextPath = getConfig().publicRuntimeConfig.contextPath;
 
   const containerClassName = classNames(
-    "surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden",
+    "surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden w-full",
     { "p-input-filled": layoutConfig.inputStyle === "filled" }
   );
 
   return (
     <div className={containerClassName}>
-      <div className="flex flex-column align-items-center justify-content-center">
+      <div className="flex flex-column align-items-center justify-content-center w-full">
         <img
           src={`${contextPath}/layout/images/logo-${
             layoutConfig.colorScheme === "light" ? "dark" : "white"
@@ -75,6 +76,7 @@ function Login() {
           className="mb-5 w-6rem flex-shrink-0"
         />
         <div
+          className="w-11 sm:w-10 md:w-7 lg:w-7 xl:w-5"
           style={{
             borderRadius: "56px",
             padding: "0.3rem",
@@ -89,74 +91,59 @@ function Login() {
             >
               <div className="text-center mb-5">
                 <div className="text-900 text-3xl font-medium mb-3">
-                  Welcome, Isabel!
+                  Welcome back!
                 </div>
                 <span className="text-600 font-medium">
                   Sign in to continue
                 </span>
               </div>
 
-              <div>
+              <div className="mb-4">
                 <Controller
                   name="email"
                   control={control}
                   rules={{ required: "Email is required." }}
-                  render={({ field, fieldState }) => (
+                  render={({ field }) => (
                     <>
-                      <label
-                        htmlFor={field.name}
-                        className="block text-900 text-xl font-medium mb-2"
-                      >
-                        Email
-                      </label>
-                      <InputText
-                        id={field.name}
-                        {...field}
-                        className="w-full md:w-30rem mb-4"
-                        style={{ padding: "1rem" }}
-                        {...register("email")}
+                      <Input
+                        name={"email"}
+                        type={"text"}
+                        feedback={false}
+                        register={register}
+                        field={field}
+                        label={"Email"}
+                        errors={errors.email?.message}
                       />
-                      <p>{JSON.stringify(errors.email?.message)}</p>
                     </>
                   )}
                 />
-
-                <div>
-                  <Controller
-                    control={control}
-                    name="password"
-                    rules={{ required: "Password is required." }}
-                    render={({ field: { ref, ...field }, fieldState }) => (
-                      <>
-                        <label
-                          htmlFor={field.name}
-                          className="block text-900 text-xl font-medium mb-2"
-                        >
-                          Password
-                        </label>
-
-                        <Password
-                          type="password"
-                          {...register("password")}
-                          id={field.name}
-                          {...field}
-                          className="w-full mb-5"
-                          inputClassName="w-full p-3"
-                          toggleMask
-                          feedback={false}
-                        />
-                        <p>{JSON.stringify(errors.password?.message)}</p>
-                      </>
-                    )}
-                  />
-                </div>
-
-                <Button
-                  label="Sign In"
-                  className="primary w-full p-3 text-xl"
-                  type="submit"
+              </div>
+              <div className="mb-6">
+                <Controller
+                  control={control}
+                  name="password"
+                  rules={{ required: "Password is required." }}
+                  render={({ field: { ref, ...field } }) => (
+                    <>
+                      <Input
+                        name={"password"}
+                        type={"password"}
+                        feedback={false}
+                        register={register}
+                        field={field}
+                        label={"Password"}
+                        errors={errors.password?.message}
+                      />
+                    </>
+                  )}
                 />
               </div>
+
+              <Button
+                label="Sign In"
+                className="primary w-full p-3 text-xl"
+                type="submit"
+              />
             </div>
           </form>
         </div>
---
+++ src/shared/input/input.tsx
@@ -0,0 +1,53 @@
+import { InputText } from "primereact/inputtext";
+import { Message } from "primereact/message";
+import { Password } from "primereact/password";
+
+export const Input = ({
+  name,
+  field,
+  label,
+  errors,
+  register,
+  type,
+  feedback,
+}: {
+  name: any;
+  field: any;
+  label: any;
+  errors: any;
+  register: any;
+  type: any;
+  feedback: boolean;
+}) => {
+  return (
+    <>
+      <label
+        htmlFor={field.name}
+        className="block text-900 text-xl font-medium mb-2"
+      >
+        {label}
+      </label>
+      {type === "text" ? (
+        <InputText
+          id={field.name}
+          {...field}
+          className="w-full mb-1"
+          style={{ padding: "1rem" }}
+          {...register("email")}
+        />
+      ) : (
+        <Password
+          type="password"
+          {...register("password")}
+          id={field.name}
+          {...field}
+          className="w-full"
+          inputClassName="w-full p-3 mb-1"
+          toggleMask
+          feedback={false}
+        />
+      )}
+      {errors && <Message severity="error" text={errors} />}
+    </>
+  );
+};
